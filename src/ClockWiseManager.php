859904bde5599b2c707478e947aafc877aa21c62
Pedro Barros <phalmeida001@gmail.com>
2025-03-20 14:04:47 -0300

    refactor - change manager folder

--- src/ClockWiseManager.php
+++ src/ClockWiseManager.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Infrastructure\Manager;
+namespace App;
 
 use Application\UseCases\GetBatchResultUseCase;
 use Application\UseCases\GetCurrentBatchStatusUseCase;
